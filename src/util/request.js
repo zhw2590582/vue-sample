c6f7d7b4bb15546588526cc5f22ecba12e3d8627
zhaohaiwei <zhaohaiwei@xinghehudong.com>
2018-03-21 09:49:40 +0800

    update

--- src/util/request.js
+++ src/util/request.js
@@ -1,31 +1,31 @@
 /* eslint-disable */
 
 export default function request(method, url, body) {
-    method = method.toUpperCase();
-    body = body && JSON.stringify(body);
-  
-    console.log('请求方法：' + method);
-    console.log('请求接口：' + url);
-    console.log('请求数据：' + body);
-    console.log('%c----------------------------------------', 'color: blue');
-  
-    let headers = {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+  method = method.toUpperCase();
+  body = body && JSON.stringify(body);
+
+  console.log("请求方法：" + method);
+  console.log("请求接口：" + url);
+  console.log("请求数据：" + body);
+  console.log("%c----------------------------------------", "color: blue");
+
+  let headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
+
+  return fetch(url, {
+    method,
+    headers,
+    body
+  }).then(res => {
+    if (res.status === 404) {
+      return Promise.reject("Unauthorized.");
+    } else {
+      return res.json();
     }
-  
-    return fetch(url, {
-      method,
-      headers,
-      body
-    }).then(res => {
-      if (res.status === 404) {
-        return Promise.reject('Unauthorized.');
-      } else {
-        return res.json();
-      }
-    });
-  }
-  
-  export const get = url => request('GET', url, undefined);
-  export const post = (url, body) => request('POST', url, body);
+  });
+}
+
+export const get = url => request("GET", url, undefined);
+export const post = (url, body) => request("POST", url, body);
